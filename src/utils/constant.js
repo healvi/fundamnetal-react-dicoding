9eeeb1d8b389489bc43871149daa1e3d68ea8a90
healvi <healvimaginer@gmail.com>
2022-09-11 18:18:23 +0700

    path

---
+++ src/utils/constant.js
@@ -0,0 +1,7 @@
+const HomePath = '/';
+const ArshipPath = '/arsip';
+const CreatePath = '/create';
+const NotFoundPath = '*';
+const DetailsPath = '/details/:id';
+
+export {HomePath, ArshipPath,CreatePath,NotFoundPath,DetailsPath}
